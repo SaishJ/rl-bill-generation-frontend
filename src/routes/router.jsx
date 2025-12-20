de653a138bd3943209824959573ea965126c4baf
devsaishj <saish.jagtap@dwebbox.com>
2025-12-20 12:41:39 +0530

    Fix: Routing Issue

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,7 +2,6 @@ import { lazy } from "react";
 import { createBrowserRouter } from "react-router";
 
 import AuthLayout from "./layout/AuthLayout.jsx";
-import AppLayout from "./layout/AppLayout.jsx";
 import ProtectedRoute from "./layout/ProtectedRoute.jsx";
 
 const Login = lazy(() => import("../pages/Login.jsx"));
@@ -19,13 +18,8 @@ const router = createBrowserRouter([
     element: <ProtectedRoute />,
     children: [
       {
-        element: <AppLayout />,
-        children: [
-          {
-            path: "/",
-            Component: Home,
-          },
-        ],
+        path: "/",
+        Component: Home,
       },
     ],
   },
